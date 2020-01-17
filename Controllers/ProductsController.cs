e31171ba3ccf91b944df72fd85dfaa69add03615
Rupinderjeet Singh <200433751@student.georgianc.on.ca>
2020-01-16 23:21:30 -0400

    Completing Lab2

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+
+
+            string[] products = { "Laptop", "Tablet", "Mobile" };
+            
+            ViewData["products"] = products;
+            
+            return View();
+         
+        }
+        public String Details(string product)
+        {
+            string message = "You have selected" +product;
+            return message;
+
+        }
+    }
+}
